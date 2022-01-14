510775b22b9ccee406c326ba792e8dda334f9534
PNahuel <86082421+PNahuel@users.noreply.github.com>
2022-01-14 17:38:33 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function add(product, price){
 console.log(product, price);
 products.push(product);
 total = total + price;
-
+document.getElementById("checkout").innerHTML = `Pagar $${total}`
 
 }
 
